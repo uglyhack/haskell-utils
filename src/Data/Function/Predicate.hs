e339a538d77547c65b7c8cf9c01eb5bab04f4bc9
Dan P <code@nospam.syn-com.de>
2013-05-16 02:01:20 +0200

    Rename fromPredicates to fromMany (module Data.Function.Predicate).

--- src/Data/Function/Predicate.hs
+++ src/Data/Function/Predicate.hs
@@ -1,11 +1,10 @@
 module Data.Function.Predicate
   where
 
--- |The 'fromPredicates' function builds a predicate from a list of predicates
---  and uses the function 'f' to fold the results.
-fromPredicates :: ([Bool] -> Bool) -- ^Function 'f' is used to fold the results.
-               -> [a -> Bool]      -- ^List of predicates 'ps', that form the
-                                   --  resulting predicate function
-               -> (a -> Bool)
-fromPredicates f ps = \x -> f $ map ($ x) ps
+-- |'fromMany' builds a predicate from a list of predicates.
+fromMany :: ([Bool] -> Bool) -- ^Function 'f' is used to fold the results.
+         -> [a -> Bool]      -- ^List of predicates 'ps', that form the
+                             --  resulting predicate function
+         -> (a -> Bool)
+fromMany f ps = \x -> f $ map ($ x) ps
 
